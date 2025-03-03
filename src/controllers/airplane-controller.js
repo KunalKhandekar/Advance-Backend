9177919cea3c7a924d6cc77ad132817b4fe83df8
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-03-03 17:39:56 +0530

    Managed the JSON Response for Error and Success

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,34 +1,29 @@
 const { error } = require("winston");
 const { AirplaneSerivce } = require("../services");
-const {StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
+
+const { ErrorResponse, SuccessResponse } = require("../utils/common");
 
 /**
  POST : /airplanes
  req-body : { modelNumber: 'airbus230', capacity: 200 } 
  **/
 async function createAirplane(req, res) {
-    try {
-        const airplane =  await AirplaneSerivce.createAirplane({
-            modelNumber: req.body.modelNumber,
-            capacity: req.body.capacity
-        });
-
-        return res.status(StatusCodes.CREATED).json({
-            success: true,
-            message: "Successfully created an airplane",
-            data: airplane,
-            error: {}
-        })
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            success: false,
-            message: "Something went wrong while creating airplane",
-            data: {},
-            error: error
-        })
-    }
-};
+  try {
+    const airplane = await AirplaneSerivce.createAirplane({
+      modelNumber: req.body.modelNumber,
+      capacity: req.body.capacity,
+    });
+    SuccessResponse.message = "Successfully created an airplane";
+    SuccessResponse.data = airplane;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.message = "Something went wrong while creating airplane";
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
 
 module.exports = {
-    createAirplane
-}
+  createAirplane,
+};
--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { airplaneController } = require("../../controllers");
+const { AirplaneMiddleware } = require("../../middlewares");
 const airplaneRoutes = express.Router();
 
 // POST -> /api/v1/airplanes
-airplaneRoutes.post('/', airplaneController.createAirplane)
+airplaneRoutes.post(
+  "/",
+  AirplaneMiddleware.validateCreateRequest,
+  airplaneController.createAirplane
+);
 
 module.exports = airplaneRoutes;
